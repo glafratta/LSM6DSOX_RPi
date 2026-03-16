7ddcf760f1fa04ce658adfe22efd34f2f41e3099
glafratta <g.lafratta@outlook.com>
2026-03-16 00:06:47 +0000

    compiles but doesn't do anything and doesn't get any data

--- LSM6DSOX.cpp
+++ LSM6DSOX.cpp
@@ -2,26 +2,61 @@
 
 void LSM6DSOX::start(){
     chipDRDY=gpiod_chip_open(device.c_str());
-    pinDRDY=gpiod_chip_get_line(chipDRDY, LMS6DSOX_DRDY_GPIO);
-    int ret=gpiod_line_request_rising_edge_events(pinDRDY, "Consumer"); //request rising edge events not. on a single line
-    if (ret<0)
+    settings=gpiod_line_settings_new();
+    gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_INPUT); //defined in gpiod.h
+    gpiod_line_settings_set_edge_detection(settings, GPIOD_LINE_EDGE_RISING); //defined in gpiod.h
+    line_cfg = gpiod_line_config_new();
+    gpiod_line_config_add_line_settings(line_cfg, &LMS6DSOX_DRDY_GPIO, 1, settings); //offset is the gpio pin (22)
+    req_cfg = gpiod_request_config_new();
+    gpiod_request_config_set_consumer(req_cfg, "imu");
+    request = gpiod_chip_request_lines(chip, req_cfg, line_cfg);
+    //pinDRDY=gpiod_chip_get_line_info(chipDRDY, LMS6DSOX_DRDY_GPIO);
+    //v1, need to rewrite in v2
+   // int ret=gpiod_line_request_rising_edge_events(pinDRDY, "Consumer"); //request rising edge events not. on a single line
+    if (!request){
+        gpiod_line_request_release(request);
+        throw "Could not request event";
+    }
+    thread=std::thread(&LSM6DSOX::worker, this);
 }
 
 void LSM6DSOX::worker(){
     running=true;
     while (running){
-        const struct timespec ts={1,0};
-        int r=gpiod_line_event_wait(pinDRDY, &ts);
-        if (r==1){
-            struct gpiod_line_event event;
-            gpiod_line_event_read(pinDRDY, &event);
-            //callback goes here
+        int r=gpiod_line_request_wait_edge_events(request, -1);
+        if (r){
+            // struct gpiod_line_event event;
+            // gpiod_line_event_read(pinDRDY, &event);
+            struct gpiod_edge_event_buffer *buffer;
+            buffer = gpiod_edge_event_buffer_new(1);
+            int n = gpiod_line_request_read_edge_events(request, buffer, 1);
+            if (n){
+                //callback goes here
+                printf("Interrupt happened!");
+            }
+            gpiod_edge_event_buffer_free(buffer);
         }
         else{
             running=false;
         }
     }
 
+}
+
+void LSM6DSOX::stop(){
+    running = false;
+    if (thread.joinable()) {
+	    thread.join();
+    }
+    if (NULL != request) {
+	    //gpiod_line_release(pinDRDY);
+        gpiod_line_request_release(request);
+    }
+    if (NULL != chipDRDY) {
+	    gpiod_chip_close(chipDRDY);
+    }
+    // pinDRDY = NULL;
+    // chipDRDY = NULL;
 }
 
 // int LSM6DSOX::i2cOpen(){
--- LSM6DSOX.h
+++ LSM6DSOX.h
@@ -3,19 +3,19 @@
 #include <gpiod.h>
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
-#include <lsm6dsox_reg.h>
+#include "lsm6dsox_reg.h"
+#include "LSM6DSOXSample.h"
+#include <string>
 #ifndef LSM6DSOX_H
 #define LSM6DSOX_H
 
-const int LMS6DSOX_DRDY_GPIO=22; // RPi physical pin 15
+const unsigned int LMS6DSOX_DRDY_GPIO=22; // RPi physical pin 15
 
 class LSM6DSOX{
 
 
     public:     
-    LSM6DSOX(const std:string& _device="/dev/i2c-1", uint8_t _address=LSM6DSOX_ID):device(_device), address(_address){
-        
-    }
+    LSM6DSOX(const std::string& _device="/dev/i2c-1", uint8_t _address=LSM6DSOX_ID):device(_device), address(_address){}
 
     ~LSM6DSOX();
 
@@ -25,15 +25,29 @@ class LSM6DSOX{
 
     void worker();
 
+    struct LSM6DSOXCallback{ //abstract
+        virtual void hasSample(LSM6DSOXSample sample)=0; 
+    };
+
     private:
-    std::string device;
-    uint8_t address=0x0;//null
+    std::string device; //device file
+    uint8_t address=0;// chip address
     std::thread thread;
 
     bool running=false;
 
+    /**
+    * v1 defs for reference
+    */
     struct gpiod_chip *chipDRDY = nullptr;
-    struct gpiod_line* pinDRDY=nullptr;    
+    // struct gpiod_line* pinDRDY=nullptr;    
+
+    struct gpiod_chip *chip=nullptr;
+    struct gpiod_line_settings *settings=nullptr;
+    struct gpiod_line_config *line_cfg=nullptr;
+    struct gpiod_request_config *req_cfg=nullptr;
+    struct gpiod_line_request *request=nullptr;
+
 
    // int i2cOpen();
     
