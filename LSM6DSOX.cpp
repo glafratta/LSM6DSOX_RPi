4b34d8d7d0aa31ae6c9d30d01dbf8ffa2678955c
glafratta <g.lafratta@outlook.com>
2026-03-25 19:41:00 +0000

    including smbus to do read byte data, in case 1 byte read is not supported?

--- LSM6DSOX.cpp
+++ LSM6DSOX.cpp
@@ -111,15 +111,19 @@ int LSM6DSOX::i2cOpen(int address){
 
 uint8_t LSM6DSOX::i2cReadByte(uint8_t address){
     int fd=i2cOpen(address);
-    uint8_t tmp[2];
-    tmp[0]=address;
-    long int w=write(fd, &tmp, 1);
-    long int r= read(fd, tmp, 1);
+    uint8_t buffer[1];
+    buffer[0]=address;
+    // long int w=write(fd, tmp, 1);
+    // if (w<0){
+    //     perror("Write failed!\n");
+    // }
+   // uint8_t r= read(fd, buffer, 1); //read one byte and write to tmp buffer
+    long int r=i2c_smbus_read_byte_data(fd, address);
     if (r<0){
         std::cout<< "Can't read from i2c"<<std::endl;
     }
     close(fd);
-    return tmp[0];
+    return buffer[0];
 }
 
 
